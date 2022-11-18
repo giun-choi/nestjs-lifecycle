e38546c4656a57938609c4db9274241ea3446840
giun-choi <liamg8341@gmail.com>
2022-11-19 08:12:17 +0900

    set passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
+import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, UsersModule],
+  providers: [AuthService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
